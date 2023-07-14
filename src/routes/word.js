71e13805189b6dace5f591cce3a97a03a4197b0c
luongaimy <luongaimy@gmail.com>
2023-07-14 14:25:51 +0200

    CRUD of Word

--- src/routes/word.js
+++ src/routes/word.js
@@ -6,7 +6,7 @@ const WordModel = require('../models/word');
 Router.post('/', async (request, response) => {
     const { word } = request.body;
 
-    const wordModel = new WordModel({ 
+    const wordModel = new WordModel({
         name: word
     });
 
@@ -16,6 +16,53 @@ Router.post('/', async (request, response) => {
 
         return response.status(200).json({
             "msg": word
+        });
+
+    } catch (error) {
+        return response.status(500).json({
+            "error": error.message
+        });
+    }
+});
+
+// READ
+Router.get('/:id', async (request, response) => {
+    try {
+        const word = await WordModel.findById(request.params.id);
+        if (!word) return response.status(404).json({ msg: 'Word not found' });
+
+        return response.status(200).json(word);
+
+    } catch (error) {
+        return response.status(500).json({
+            "error": error.message
+        });
+    }
+});
+
+// UPDATE
+Router.put('/:id', async (request, response) => {
+    try {
+        const updatedWord = await WordModel.findByIdAndUpdate(request.params.id, { name: request.body.word }, { new: true });
+        if (!updatedWord) return response.status(404).json({ msg: 'Word not found' });
+
+        return response.status(200).json(updatedWord);
+
+    } catch (error) {
+        return response.status(500).json({
+            "error": error.message
+        });
+    }
+});
+
+// DELETE
+Router.delete('/:id', async (request, response) => {
+    try {
+        const deletedWord = await WordModel.findByIdAndRemove(request.params.id);
+        if (!deletedWord) return response.status(404).json({ msg: 'Word not found' });
+
+        return response.status(200).json({
+            "msg": "Word deleted successfully"
         });
 
     } catch (error) {
